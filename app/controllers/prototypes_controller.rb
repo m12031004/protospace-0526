3c24a8c603d1cb79f6541548d12f0f552c3c2ae7
m12031004 <misaki_tsuchimoto@mail.ncn-ncc.co.jp>
2026-05-28 11:06:51 +0900

    アソシエーション記入

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,4 +3,8 @@ class PrototypesController < ApplicationController
   def index
     @prototypes = Prototype.all
   end
+
+  def new
+    @prototype = Prototype.new
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_05_26_075828) do
+ActiveRecord::Schema[7.1].define(version: 2026_05_28_014442) do
   create_table "prototypes", charset: "utf8mb3", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "catch_copy", null: false
+    t.text "concept", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_prototypes_on_user_id"
   end
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
@@ -32,4 +37,5 @@ ActiveRecord::Schema[7.1].define(version: 2026_05_26_075828) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "prototypes", "users"
 end
